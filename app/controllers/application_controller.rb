32be7852a74da936bcff485396f2bbbc9e68af5b
toshikioi0601 <toshikioi0601@gmail.com>
2021-04-22 22:22:16 +0000

    検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
   protect_from_forgery with: :exception
   include SessionsHelper
-  
+
+  def set_search
+    if logged_in?
+      @search_word = params[:q][:name_cont] if params[:q]
+      @q = current_user.feed.paginate(page: params[:page], per_page: 5).ransack(params[:q])
+      @divelogs = @q.result(distinct: true)
+    end
+  end
+
   private
-  
+
   def logged_in_user # ログイン済みユーザーかどうか確認する用
     unless logged_in?
       store_location
